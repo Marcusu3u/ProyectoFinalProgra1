f7e49a3ef9c50cf93d02633a31fa0cac18724629
Marcusu3u <brandon.vegae@gmail.com>
2025-10-28 11:32:26 -0600

    Clase pasajero Jaylinne

--- src/clases/Pasajero.java
+++ src/clases/Pasajero.java
@@ -1,9 +1,72 @@
 package clases;
 
 public class Pasajero {
+	
+	 private String nombre;
+	    private String apellido;
+	    private String cedula;
+	    private String telefono;
+	    private String correo;
 
-	public Pasajero() {
-		// TODO Auto-generated constructor stub
-	}
+	    // Constructor
+	    public Pasajero(String nombre, String apellido, String cedula, String telefono, String correo) {
+	        this.nombre = nombre;
+	        this.apellido = apellido;
+	        this.cedula = cedula;
+	        this.telefono = telefono;
+	        this.correo = correo;
+	    }
 
+	    // Getter
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    public String getApellido() {
+	        return apellido;
+	    }
+
+	    public void setApellido(String apellido) {
+	        this.apellido = apellido;
+	    }
+
+	    public String getCedula() {
+	        return cedula;
+	    }
+
+	    public void setCedula(String cedula) {
+	        this.cedula = cedula;
+	    }
+
+	    public String getTelefono() {
+	        return telefono;
+	    }
+
+	    public void setTelefono(String telefono) {
+	        this.telefono = telefono;
+	    }
+
+	    public String getCorreo() {
+	        return correo;
+	    }
+
+	    public void setCorreo(String correo) {
+	        this.correo = correo;
+	    }
+	    
+	    public String toString () {
+	    	
+	    	return "Nombre: "+nombre +
+	    			"Apellido: "+apellido+
+	    			"Cedula: "+cedula+
+	    			"telefono: "+telefono+
+	    			"Correo: "+correo;
+	    			
+	    			
+	    }
+	
 }
