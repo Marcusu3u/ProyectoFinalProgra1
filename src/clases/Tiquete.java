df9578b01e5cc5a4169231da0578b74e647d577f
Gricelda Arcia <pazgriss3108@gmail.com>
2025-11-06 00:30:22 -0600

    Implementacion de clases Vuelo y Tiquete

--- src/clases/Tiquete.java
+++ src/clases/Tiquete.java
@@ -1,9 +1,60 @@
 package clases;
 
 public class Tiquete {
+    
+    private String numeroTiquete;
+    private Pasajero pasajero;
+    private Vuelo vuelo;     
+    private String clase;     
+    private double montoTotal;
 
-	public Tiquete() {
-		// TODO Auto-generated constructor stub
-	}
+    //Constructor
+    public Tiquete(String numeroTiquete, Pasajero pasajero, Vuelo vuelo, String clase) {
+        this.numeroTiquete = numeroTiquete;
+        this.pasajero = pasajero;
+        this.vuelo = vuelo;
+        this.clase = clase;
+        
+        //Calcular el monto total
+        if (clase.equalsIgnoreCase("ejecutiva")) {
+            this.montoTotal = vuelo.getPrecioEjecutiva();
+        } else if (clase.equalsIgnoreCase("económica")) {
+            this.montoTotal = vuelo.getPrecioEconomica();
+        } else {
+            this.montoTotal = 0.0;
+        }
+    }
 
+    //Getters
+    public String getNumeroTiquete() {
+        return numeroTiquete;
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+
+    public Vuelo getVuelo() {
+        return vuelo;
+    }
+
+    public String getClase() {
+        return clase;
+    }
+
+    public double getMontoTotal() {
+        return montoTotal;
+    }
+
+    //Método toString
+    @Override
+    public String toString() {
+        return "\n--- Tiquete de Vuelo ---\n" +
+               "Número de Tiquete: " + numeroTiquete + "\n" +
+               "Vuelo: " + vuelo.getOrigen() + " -> " + vuelo.getDestino() + "\n" +
+               "Clase: " + clase + "\n" +
+               "Pasajero: " + pasajero.getNombre() + " " + pasajero.getApellido() + "\n" +
+               "Identificación: " + pasajero.getCedula() + "\n" +
+               "Costo: €" + String.format("%.2f", montoTotal);
+    }
 }
--- src/clases/Vuelo.java
+++ src/clases/Vuelo.java
@@ -2,8 +2,95 @@ package clases;
 
 public class Vuelo {
 
-	public Vuelo() {
-		// TODO Auto-generated constructor stub
-	}
+ private String codigoVuelo;
+ private String origen;
+ private String destino;
+ private double precioEconomica;
+ private double precioEjecutiva;
+ 
+ private int asientosDisponiblesEjecutiva; 
+ private int asientosDisponiblesEconomica; 
 
+ //Constructor
+ public Vuelo(String codigoVuelo, String origen, String destino, double precioEconomica, double precioEjecutiva) {
+     this.codigoVuelo = codigoVuelo;
+     this.origen = origen;
+     this.destino = destino;
+     this.precioEconomica = precioEconomica;
+     this.precioEjecutiva = precioEjecutiva;
+ //Inicializar 
+     this.asientosDisponiblesEjecutiva = 2;
+     this.asientosDisponiblesEconomica = 2;
+ }
+
+ //Getters
+ public String getCodigoVuelo() {
+     return codigoVuelo;
+ }
+
+ public String getOrigen() {
+     return origen;
+ }
+
+ public String getDestino() {
+     return destino;
+ }
+
+ public double getPrecioEconomica() {
+     return precioEconomica;
+ }
+
+ public double getPrecioEjecutiva() {
+     return precioEjecutiva;
+ }
+ 
+ public int getAsientosDisponiblesEjecutiva() {
+     return asientosDisponiblesEjecutiva;
+ }
+
+ public int getAsientosDisponiblesEconomica() {
+     return asientosDisponiblesEconomica;
+ }
+
+ //Setters
+ public void setAsientosDisponiblesEjecutiva(int asientosDisponiblesEjecutiva) {
+     this.asientosDisponiblesEjecutiva = asientosDisponiblesEjecutiva;
+ }
+
+ public void setAsientosDisponiblesEconomica(int asientosDisponiblesEconomica) {
+     this.asientosDisponiblesEconomica = asientosDisponiblesEconomica;
+ }
+
+
+ public boolean verificarDisponibilidad(String clase) {
+     if (clase.equalsIgnoreCase("ejecutiva")) {
+         return this.asientosDisponiblesEjecutiva > 0;
+     } else if (clase.equalsIgnoreCase("económica")) { 
+         return this.asientosDisponiblesEconomica > 0;
+     }
+     return false;
+ }
+ 
+ public boolean reservarAsiento(String clase) {
+     if (verificarDisponibilidad(clase)) {
+          if (clase.equalsIgnoreCase("ejecutiva")) {
+             this.asientosDisponiblesEjecutiva--;
+             return true;
+         } else if (clase.equalsIgnoreCase("económica")) {
+             this.asientosDisponiblesEconomica--;
+             return true;
+         }
+     }
+     return false;
+ }
+ 
+ //Método toString
+ @Override
+ public String toString() {
+     return "Vuelo: " + codigoVuelo + 
+            " | Origen: " + origen + 
+            " | Destino: " + destino +
+            " | Disp. Ejecutiva: " + asientosDisponiblesEjecutiva +
+            " | Disp. Económica: " + asientosDisponiblesEconomica;
+ }
 }
