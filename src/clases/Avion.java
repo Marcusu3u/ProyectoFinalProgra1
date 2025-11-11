4bd8ca8786218f3e6c553ca4f9520901504e06d2
SantiagoB <santiiobucareo@gmail.com>
2025-11-11 17:54:55 -0600

    Clase factura lista

--- src/clases/Avion.java
+++ src/clases/Avion.java
@@ -1,9 +1,85 @@
 package clases;
 
 public class Avion {
-
-	public Avion() {
-		// TODO Auto-generated constructor stub
+	//Sus respectivos atributos .|.
+	private String codigoAvion;
+	private String modelo;
+	private int capacidadEjecutiva = 2;
+	private int capacidadEconomica = 2;
+	private int asientosDisponiblesEjecutivos;
+	private int asientosDisponiblesEconomicos;
+	
+	/*Su respectivo constructor lleno
+	 * (Si en algun momento se necesita algun constructor que solo valida y o dos atributos, se crea)
+	 
+	 */
+	public Avion(String codigoAvion, String modelo, int capacidadEjecutiva, int capacidadEconomica,
+			int asientosDisponiblesEjecutivos, int asientosDisponiblesEconomicos) {
+		this.codigoAvion = codigoAvion;
+		this.modelo = modelo;
+		this.capacidadEjecutiva = capacidadEjecutiva;
+		this.capacidadEconomica = capacidadEconomica;
+		this.asientosDisponiblesEjecutivos = asientosDisponiblesEjecutivos;
+		this.asientosDisponiblesEconomicos = asientosDisponiblesEconomicos;
 	}
+	
+	
+	//Sus respectivos setters and getters
+	public String getCodigoAvion() {
+		return codigoAvion;
+	}
+	public void setCodigoAvion(String codigoAvion) {
+		this.codigoAvion = codigoAvion;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getCapacidadEjecutiva() {
+		return capacidadEjecutiva;
+	}
+	public void setCapacidadEjecutiva(int capacidadEjecutiva) {
+		this.capacidadEjecutiva = capacidadEjecutiva;
+	}
+	public int getCapacidadEconomica() {
+		return capacidadEconomica;
+	}
+	public void setCapacidadEconomica(int capacidadEconomica) {
+		this.capacidadEconomica = capacidadEconomica;
+	}
+	public int getAsientosDisponiblesEjecutivos() {
+		return asientosDisponiblesEjecutivos;
+	}
+	public void setAsientosDisponiblesEjecutivos(int asientosDisponiblesEjecutivos) {
+		this.asientosDisponiblesEjecutivos = asientosDisponiblesEjecutivos;
+	}
+	public int getAsientosDisponiblesEconomicos() {
+		return asientosDisponiblesEconomicos;
+	}
+	public void setAsientosDisponiblesEconomicos(int asientosDisponiblesEconomicos) {
+		this.asientosDisponiblesEconomicos = asientosDisponiblesEconomicos;
+	}
+	
+	//Metodos en funcion :/
+	
+	public boolean verificarDisponibilidad(String clase) {
+		if (clase.equalsIgnoreCase("Ejecutiva")) {
+			if (asientosDisponiblesEjecutivos <= 0) {
+				return false;
+			}
+		} else if (clase.equalsIgnoreCase("Economica")) {
+			if (asientosDisponiblesEconomicos <= 0) {
+				return false;
+			}
+		} else {
+			return false; // Clase no valida
+		}
+		
+		return true;
+	}
+	
+	
 
 }
--- src/clases/Factura.java
+++ src/clases/Factura.java
@@ -1,9 +1,66 @@
 package clases;
 
 public class Factura {
-
-	public Factura() {
-		// TODO Auto-generated constructor stub
+	//Atributos privados 
+	private String numeroFactura;
+	private Tiquete tiquete;
+	private double subTotal;
+	private double impuestos;
+	private double total;
+	
+	//Constructor
+	public Factura(String numeroFactura, Tiquete tiquete, double subTotal, double impuestos, double total) {
+		this.numeroFactura = numeroFactura;
+		this.tiquete = tiquete;
+		this.subTotal = subTotal;
+		this.impuestos = impuestos;
+		this.total = total;
 	}
-
+	//Respectivos getters y setters para utilizar los atributos privados
+	public String getNumeroFactura() {
+		return numeroFactura;
+	}
+	public void setNumeroFactura(String numeroFactura) {
+		this.numeroFactura = numeroFactura;
+	}
+	public Tiquete getTiquete() {
+		return tiquete;
+	}
+	public void setTiquete(Tiquete tiquete) {
+		this.tiquete = tiquete;
+	}
+	public double getSubTotal() {
+		return subTotal;
+	}
+	public void setSubTotal(double subTotal) {
+		this.subTotal = subTotal;
+	}
+	public double getImpuestos() {
+		return impuestos;
+	}
+	public void setImpuestos(double impuestos) {
+		this.impuestos = impuestos;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	//Respectivos métodos para calcular el total y mostrar la factura
+	public void calcularTotal() {
+		this.impuestos = this.subTotal * 0.13;
+		this.total = this.subTotal + this.impuestos;
+	}
+	
+	public void mostrarFactura() {
+		System.out.println("\n--- Factura ---");
+		System.out.println("Número de Factura: " + numeroFactura);
+		System.out.println(tiquete.toString());
+		System.out.printf("Subtotal: €%.2f\n", subTotal);
+		System.out.printf("Impuestos (13%%): €%.2f\n", impuestos);
+		System.out.printf("Total: €%.2f\n", total);
+	}
+	
+	
 }
